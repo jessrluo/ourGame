3dddd147fa1be39cdf6c4deadbcb4a29a0c85aa2
z5218648 <z5218648@cse.unsw.edu.au>
2018-05-05 19:52:23 +1000

    Added puns

--- calculator.c
+++ calculator.c
@@ -7,6 +7,13 @@
 
 int main (int argc, char* argv[]) {
 
+sup yall howre ya doin mateys im great 
+ahoy ahoy i c a ship can it function i dont know wow its on a stack 
+Now i gotta fix malloc so i can open the door 
+free me free me
+i sure love HEAPs of stuff in my FRAME
+
+
     return EXIT_SUCCESS;
 }
 
